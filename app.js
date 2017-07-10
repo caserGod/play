4e2af824f19696554894a2751b3cd528e6baf5fe
wkx <1326454178@qq.com>
2017-07-10 18:28:37 +0800

    oneFile

---
+++ app.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Administrator
+* @Date:   2017-07-10 18:25:20
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-07-10 18:25:39
+*/
+
+'use strict';
+
+
+var a=0;
+console.log(a)
